fae7e8ca859aeb1d5df06a84a43ca001fc8d9320
Rafael Batista <rabsouza@gmail.com>
2016-04-12 01:33:40 -0300

    [rafaelbs] Fix grid elements

--- app/src/main/java/br/com/battista/myoffers/constants/ViewConstant.java
+++ app/src/main/java/br/com/battista/myoffers/constants/ViewConstant.java
@@ -2,6 +2,6 @@ package br.com.battista.myoffers.constants;
 
 public interface ViewConstant {
 
-       Integer MAX_PRODUCTS_GRID = 9;
+       Integer MAX_PRODUCTS_GRID = 8;
        String PARAM_ID_PRODUCT = "br.com.battista.myoffers.Offer.IdProduct";
 }
